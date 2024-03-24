2749f7636be42ab547bd6fe1d0bcb838de4219c3
John <johncomstock9716@gmail.com>
2024-03-23 22:48:53 -0400

    removing bloat

--- src/snake.py
+++ src/snake.py
@@ -3,13 +3,15 @@ class Snake3D:
         self.renderer = renderer  # Assuming renderer is passed in as a parameter
         self.color = (1, 0, 0)  # Red color for the snake
         self.block_size = block_size
-        self.positions = [[0, 0, 0]]  # Initialize with a position in 3D space, with z being up
         self.model_loaded = False
 
-        self.x_change = self.block_size  # Initial movement direction along x-axis
+        self.x = 0
+        self.y = 0
+        self.z = 0
+        self.x_change = block_size
         self.y_change = 0
         self.z_change = 0
-        self.is_visible = True  # Initialize the visibility flag
+        self.positions = [[self.x, self.y, self.z]]
 
         
         # If a model_path is provided, try to load a 3D model, otherwise the snake will be drawn with cubes
@@ -29,15 +31,6 @@ class Snake3D:
             # Here the renderer's draw_cube method is used to draw each segment of the snake
             self.renderer.draw_cube(position, self.block_size, self.color)
 
-    def move(self, x_change, y_change, z_change):
-        # Update movement direction only if it's not the opposite of the current direction
-        if (x_change * self.x_change) >= 0:
-            self.x_change = x_change
-        if (y_change * self.y_change) >= 0:
-            self.y_change = y_change
-        if (z_change * self.z_change) >= 0:
-            self.z_change = z_change
-
     def is_within_boundaries(self, position):
         # You'll need to define what the boundaries are for your game.
         min_x, max_x = -10, 10  # Example boundaries
@@ -46,27 +39,11 @@ class Snake3D:
         return min_x <= x <= max_x and min_y <= y <= max_y
 
     def update(self):
-        # Calculate new head position based on the current direction
-        head_x, head_y, head_z = self.positions[-1]
-        new_head = [head_x + self.x_change, head_y + self.y_change, head_z + self.z_change]
-
-        # Check if the new head position is within the game boundaries
-        if self.is_within_boundaries(new_head):
-            # Add the new head to the snake's body
-            self.positions.append(new_head)
-            
-            # Remove the last segment of the tail
-            self.positions.pop(0)
-        else:
-            # If new head is out of boundaries, stop updating to prevent going off-screen
-            # This should probably be handled better, for example by setting a game over state
-            pass
-
-    def grow(self):
-        # When growing, we don't remove the tail segment
-        head_x, head_y, head_z = self.positions[-1]
-        new_head = [head_x + self.x_change, head_y + self.y_change, head_z + self.z_change]
-        self.positions.append(new_head)
+        self.x += self.x_change
+        self.y += self.y_change
+        self.z += self.z_change
+        self.positions.append([self.x, self.y, self.z])
+        self.positions.pop(0)  # Remove the tail's last position to simulate movement
 
     def get_state(self):
         # This method provides the current state of the snake for rendering or other purposes
